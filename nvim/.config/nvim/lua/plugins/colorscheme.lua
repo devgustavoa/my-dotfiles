88be574af5da0576258daec79e37ba5bd3b69e75
gnix0 <dev.gustavoa@gmail.com>
2026-08-22 17:05:06 -0300

    .

--- nvim/.config/nvim/lua/plugins/colorscheme.lua
+++
@@ -1,9 +0,0 @@
-return {
-    "blazkowolf/gruber-darker.nvim",
-    name = "gruber-darker",
-    lazy = false,
-    priority = 1000,
-    config = function()
-        vim.cmd.colorscheme("gruber-darker")
-    end,
-}
